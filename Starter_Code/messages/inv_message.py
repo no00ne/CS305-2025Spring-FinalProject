85ba0a8e9bd1d8b2d9bb8cbf2e34ce01b6dcdf8b
ni <810959018>
2025-05-08 14:34:32 +0800

    Delete older version of Starter_Code

--- Starter_Code/messages/inv_message.py
+++
@@ -1,5 +0,0 @@
-from utils import generate_message_id
-
-def create_inv(sender_id, block_ids):
-    # TODO: Create the JSON format of an “INV” message, which should include: {message type, sender’s ID, related blocks’ ID, and message ID}.
-    pass
